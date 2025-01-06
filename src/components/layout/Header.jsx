821727eb0498ccfc6baa77ddb3b86631e177cc43
wohligpratik <pratik.sawant@wohlig.com>
2025-01-06 12:18:21 +0530

    changes

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -1,8 +1,11 @@
-// components/Header.jsx
-import React from 'react';
-import { Button } from "@/components/components/ui/button";
+import React, { useState } from 'react';
 import { Avatar, AvatarFallback, AvatarImage } from "../components/ui/avatar";
 import ErosNow from "../../../public/assets/images/erosnow.webp"; // Import the image
+import {
+  HoverCard,
+  HoverCardContent,
+  HoverCardTrigger,
+} from '../../components/components/ui/hover-card';
 
 const Header = () => {
   return (
@@ -11,17 +14,23 @@ const Header = () => {
       <img src={ErosNow.src} alt="ErosNow" className="mr-4 w-[14%] ml-5" /> {/* Add margin-right to give space between image and next content */}
 
       <div className="relative mt-[3px] flex h-[61px] flex-grow items-center justify-around gap-2 rounded-full bg-gradient-custom-gradient px-8 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none md:flex-grow-0 md:gap-1 xl:w-[150px] xl:gap-2 text-white border border-buttonBorder !w-[100px]">
+        {/* HoverCard with Avatar */}
+        <HoverCard>
+          <HoverCardTrigger>
+            <Avatar className="ml-auto cursor-pointer">
+              <AvatarImage src="https://github.com/shadcn.png" />
+              <AvatarFallback>CN</AvatarFallback>
+            </Avatar>
+          </HoverCardTrigger>
 
-        {/* Other Content (if any) */}
-
-        {/* Avatar */}
-        <Avatar className="ml-auto">
-          <AvatarImage src="https://github.com/shadcn.png" />
-          <AvatarFallback>CN</AvatarFallback>
-        </Avatar>
-
+          <HoverCardContent className="bg-card-cardCustomBlue">
+            <div>
+              <p className='pb-3'>Login</p>
+              <p>Dark Mode</p>
+            </div>
+          </HoverCardContent>
+        </HoverCard>
       </div>
-
     </div>
   );
 };
