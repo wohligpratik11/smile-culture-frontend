d558bf4702d2f4c86c235fca1df60b6c3a75a925
wohligpratik <pratik.sawant@wohlig.com>
2025-01-07 10:06:35 +0530

    changes

--- src/components/layout/Header.jsx
+++ src/components/layout/Header.jsx
@@ -12,20 +12,20 @@ import Cookies from 'js-cookie';
 const Header = () => {
   const router = useRouter();
 
-  const [userEmail, setUserEmail] = useState(null);
-  console.log("userEmail", userEmail)
+  const [userEmail, setUserEmail] = useState(null); 
   useEffect(() => {
     const userData = Cookies.get('userData');
     if (userData) {
       const parsedData = JSON.parse(userData);
-      setUserEmail(parsedData?.user_email);
+      setUserEmail(parsedData);
     }
   }, []);
+
   const getFirstLetter = (email) => {
     if (email) {
-      return email.charAt(0).toUpperCase();
+      return email.charAt(0).toUpperCase(); 
     }
-    return '';
+    return ''; 
   };
   const handleLoginClick = () => {
     router.push('/auth/login');
@@ -36,7 +36,7 @@ const Header = () => {
       <img src={ErosNow.src} alt="ErosNow" className="mr-4 w-[14%] ml-5" />
       <div className="relative mt-[3px] flex h-[61px] flex-grow items-center justify-around gap-2 rounded-full bg-gradient-custom-gradient px-8 py-2 shadow-xl shadow-shadow-500 dark:!bg-navy-800 dark:shadow-none md:flex-grow-0 md:gap-1 xl:w-[150px] xl:gap-2 text-white border border-buttonBorder !w-[100px]">
         <Avatar className="ml-auto cursor-pointer text-4xl">
-          <AvatarFallback >{userEmail ? getFirstLetter(userEmail) : 'A'}</AvatarFallback>
+          <AvatarFallback>{userEmail ? getFirstLetter(userEmail.user_email) : 'EROSNOW'}</AvatarFallback>
         </Avatar>
         {/* HoverCard with Avatar */}
         {/* <HoverCard>
