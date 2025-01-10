c41f5425da50bdd7f68b6291a477b59b275cc1b2
sohan1 <sohan@wohlig.com>
2025-01-10 12:44:46 +0530

    borderImageLine

--- tailwind.config.js
+++ tailwind.config.js
@@ -13,7 +13,7 @@ module.exports = {
       },
       borderImage: {
         'custom-gradient':
-          'conic-gradient(from 180deg at 51.95% 49.81%, rgba(0, 0, 0, 0.105455) -2.11deg, rgba(51, 66, 255, 0) 131.45deg, #37F4F4 175.58deg, rgba(51, 66, 255, 0) 252.32deg, rgba(0, 0, 0, 0.0885149) 310.85deg, rgba(0, 0, 0, 0.105455) 357.89deg, rgba(51, 66, 255, 0) 491.45deg), linear-gradient(0deg, #FF5733, #FF5733)',
+          'conic-gradient(from 180deg at 51.95% 49.81%, rgba(0, 0, 0, 0.105) -2.11deg, rgba(51, 66, 255, 0) 131.45deg, #37F4F4 175.58deg, rgba(51, 66, 255, 0) 252.32deg, rgba(0, 0, 0, 0.088) 310.85deg, rgba(0, 0, 0, 0.105) 357.89deg, rgba(51, 66, 255, 0) 491.45deg), linear-gradient(0deg, #FF5733, #FF5733)',
       },
       spacing: {
         913: '913px',
@@ -22,20 +22,19 @@ module.exports = {
       colors: {
         deepNavy: '#434E80',
         customWhite: '#F5F5F5',
-        buttonBorder: ' #56e5f6',
+        buttonBorder: '#56e5f6',
         vividSkyBlue: '#10A3E7',
-        lavenderBlue: '##5E6DA7',
+        lavenderBlue: '#5E6DA7',
         slateBlue: '#5B6BAB',
         blueYonder: '#495583',
-        button: {},
         card: {
-          cardCustomBlue: 'rgba(31,44,100,0.7)', // Define the transparent color
-          DEFAULT: '#ffffff', // Light mode card color
-          dark: '#1f2937', // Dark mode card color
+          DEFAULT: '#ffffff',
+          dark: '#1f2937',
+          cardCustomBlue: 'rgba(31,44,100,0.7)', // Transparent color
         },
         'card-foreground': {
-          DEFAULT: '#000000', // Light mode text color
-          dark: '#ffffff', // Dark mode text color
+          DEFAULT: '#000000',
+          dark: '#ffffff',
         },
         sidebar: {
           DEFAULT: 'hsl(var(--sidebar-background))',
@@ -50,20 +49,12 @@ module.exports = {
       },
       keyframes: {
         'accordion-down': {
-          from: {
-            height: '0',
-          },
-          to: {
-            height: 'var(--radix-accordion-content-height)',
-          },
+          from: { height: '0' },
+          to: { height: 'var(--radix-accordion-content-height)' },
         },
         'accordion-up': {
-          from: {
-            height: 'var(--radix-accordion-content-height)',
-          },
-          to: {
-            height: '0',
-          },
+          from: { height: 'var(--radix-accordion-content-height)' },
+          to: { height: '0' },
         },
       },
       animation: {
