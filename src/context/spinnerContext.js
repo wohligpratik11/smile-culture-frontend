0cadf083e4d59fbab3c3055bfad0bc69a379c83d
wohligpratik <pratik.sawant@wohlig.com>
2025-01-16 14:59:24 +0530

    changes

--- src/context/spinnerContext.js
+++ src/context/spinnerContext.js
@@ -1,50 +1,25 @@
-import React, { createContext, useContext, useState, useMemo } from 'react';
+import React, { createContext, useState, useContext } from 'react';
 
-/**
- * @typedef {Object} SpinnerContextValue
- * @property {Function} showSpinner - Function to show the spinner.
- * @property {Function} hideSpinner - Function to hide the spinner.
- */
-
-/**
- * Context for managing the spinner state.
- * @type {React.Context<SpinnerContextValue>}
- */
+// Create a context for the spinner
 const SpinnerContext = createContext();
 
-/**
- * Provider component for managing the spinner state.
- * @param {Object} props - Component props.
- * @param {React.ReactNode} props.children - The child components.
- * @returns {React.ReactNode} The spinner provider component.
- */
+export const useSpinner = () => {
+  return useContext(SpinnerContext);
+};
+
 export const SpinnerProvider = ({ children }) => {
-  /**
-   * State to manage the loading state of the spinner.
-   * @type {[boolean, Function]} - Tuple containing the loading state and function to set the loading state.
-   */
   const [loading, setLoading] = useState(false);
 
-  /**
-   * Function to show the spinner.
-   */
   const showSpinner = () => setLoading(true);
-
-  /**
-   * Function to hide the spinner.
-   */
   const hideSpinner = () => setLoading(false);
 
-  // Memoize the context value to prevent unnecessary re-renders
-  const contextValue = useMemo(() => ({ showSpinner, hideSpinner }), []);
-
   return (
-    <SpinnerContext.Provider value={contextValue}>
+    <SpinnerContext.Provider value={{ showSpinner, hideSpinner }}>
       {children}
+
       {loading && (
         <div
-          className="fixed inset-0 z-50 flex items-center justify-center"
-          style={{ backgroundColor: 'rgba(0, 0, 0, 0.3)' }}
+          className="fixed inset-0 z-50 flex items-center justify-center bg-transparent"
           aria-live="assertive"
           role="status"
         >
@@ -54,9 +29,3 @@ export const SpinnerProvider = ({ children }) => {
     </SpinnerContext.Provider>
   );
 };
-
-/**
- * Hook for accessing the spinner context.
- * @returns {SpinnerContextValue} The spinner context value containing the showSpinner and hideSpinner functions.
- */
-export const useSpinner = () => useContext(SpinnerContext);
