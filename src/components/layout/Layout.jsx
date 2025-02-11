909832338bd77aa0293902d5866f419d6eb45362
wohligpratik <pratik.sawant@wohlig.com>
2025-02-11 14:13:10 +0530

    hide scrollbar in home

--- src/components/layout/Layout.jsx
+++ src/components/layout/Layout.jsx
@@ -2,22 +2,23 @@
 
 import React from 'react';
 import Header from './Header';
+import Footer from './Footer';
+
 import ToastProvider from '../../context/ToastProvider';
 
 const Layout = ({ children }) => {
 	return (
-
-		<div className="min-h-screen flex flex-col">
+		<div className="min-h-screen flex flex-col overflow-hidden">
 			<ToastProvider>
 				<div className="mr-5 mt-4">
 					<Header />
 				</div>
-				<div>
-					<main className="flex-1">{children}</main>
+				<div className="flex-1 overflow-hidden">
+					<main>{children}</main>
 				</div>
+				{/* <Footer /> */}
 			</ToastProvider>
-
-		</div >
+		</div>
 	);
 };
 
